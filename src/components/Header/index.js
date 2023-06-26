8707188b65e1e1905d6f3a9f2ec339c621a1cbe6
Alex Kevakian <akeva001@ucr.edu>
2023-06-26 11:53:51 -0700

    Integrate Metamask signing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,45 @@
 import React, { useEffect, useState } from 'react';
 // reactstrap components
 import { Row, Col } from 'reactstrap';
-import { Link} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 import './index.scss';
 function HomePage() {
-
   const [isConnected, setIsConnected] = useState(false);
+  const [accounts, setAccounts] = useState([]);
 
   useEffect(() => {
     // Check if Metamask is installed
     if (typeof window.ethereum == 'undefined') {
-      alert("No Metamask!")
+      alert('No Metamask!');
     }
   }, []);
 
   const connectWallet = async () => {
     try {
       // Request access to the user's Metamask wallet
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const requestedAccounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setAccounts(requestedAccounts);
       setIsConnected(true);
     } catch (error) {
       console.error('Failed to connect to wallet:', error);
+    }
+  };
+
+  const signMessage = async () => {
+    if (isConnected && accounts.length > 0) {
+      try {
+        const message = 'Hello, Chainfuse!';
+        const signature = await window.ethereum.request({
+          method: 'personal_sign',
+          params: [message, accounts[0]]
+        });
+        console.log('Signed message:', signature);
+      } catch (err) {
+        console.log('Error signing message:', err);
+      }
+    } else {
+      alert('Please connect to Metamask first');
     }
   };
 
@@ -32,7 +49,7 @@ function HomePage() {
         {' '}
         <img src={require('assets/img/logo.png')} />{' '}
       </Col>
-      <Col xs="4" className="logo" >
+      <Col xs="4" className="logo">
         <Link to="/" className="margin-12">
           HOME
         </Link>{' '}
@@ -45,10 +62,11 @@ function HomePage() {
           LOGIN
         </Link>
       </Col>
-      <Col xs="4" className="logo" >
+      <Col xs="4" className="logo">
         <a className="margin-12" onClick={connectWallet}>
           {isConnected ? 'Connected' : 'Connect Wallet'}
         </a>
+        <button onClick={signMessage}>Sign Message</button>
       </Col>
     </Row>
   );
